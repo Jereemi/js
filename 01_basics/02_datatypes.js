0ae2e14255c85fbd74b8201ef765dced70970d2e
Jerry <jereemibentham@gmail.com>
2024-06-16 00:14:33 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+/**
+ * Summary of datatypes
+ * Primitive & non-primitive types
+ * null -- typeof(null) is an object
+ */
+
+//Primitive types
+
+let name = "jereemi" //string
+let age = 40 //number 
+//bigint -- especially used in stock market where large numbers are required
+let isMarried = false //boolean
+let temp = null //null -- this is a standalone value
+let state; //undefined
+//symbol -- uniqueness (will have more uses with components in react or other js frameworks)
+
+//Non-Primitive types
+//object
+
+//if you want to check the type of any variable
+console.log(typeof(temp));
